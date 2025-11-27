76c9f630a9bb8e9984f5e63be8b855cfe7a6989a
David <davkotelnikov@yandex.ru>
2025-11-27 18:16:10 +0000

    update

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ from datetime import datetime
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import ContentType
-from aiogram.utils import executor
+from aiogram.filters import Command
 
-TOKEN = "TOKEN"
-CHANNEL_ID = -100123456789
+TOKEN = "8535684697:AAH4aMEsE6P1AqPWGDwBfqhpV7tkVKrEuLA"
+CHANNEL_ID = -1003328340709
 
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher()
 
 tasks_file = "tasks.json"
 photos_dir = "photos"
@@ -29,47 +29,46 @@ def save_tasks(tasks):
     with open(tasks_file, "w", encoding="utf8") as f:
         json.dump(tasks, f, ensure_ascii=False, indent=2)
 
-# Старт команды
-@dp.message_handler(commands=["post"])
+# Словарь для хранения промежуточных данных
+user_data = {}
+
+@dp.message(Command("post"))
 async def cmd_post(message: types.Message):
     await message.reply("Кинь текст поста")
-    dp.register_message_handler(get_text, state=None, chat_id=message.chat.id)
+    user_data[message.from_user.id] = {}
+    dp.message.register(get_text, lambda m: m.from_user.id == message.from_user.id)
 
 async def get_text(message: types.Message):
-    text = message.text
+    user_data[message.from_user.id]["text"] = message.text
     await message.reply("Пришли фото или напиши 'нет', если фото не нужно")
-    
-    async def get_photo_inner(photo_msg: types.Message):
-        photo_path = None
-        if photo_msg.content_type == ContentType.PHOTO:
-            file_info = await bot.get_file(photo_msg.photo[-1].file_id)
-            downloaded_file = await bot.download_file(file_info.file_path)
-            photo_path = os.path.join(photos_dir, f"{file_info.file_id}.jpg")
-            with open(photo_path, "wb") as f:
-                f.write(downloaded_file.read())
-        await photo_msg.reply("Кинь дату и время в формате 2025-11-27 20:00")
-        
-        async def get_datetime(dt_msg: types.Message):
-            dt = datetime.strptime(dt_msg.text, "%Y-%m-%d %H:%M")
-            ts = int(dt.timestamp())
-            tasks = load_tasks()
-            tasks.append({
-                "chat_id": CHANNEL_ID,
-                "text": text,
-                "photo": photo_path,
-                "timestamp": ts
-            })
-            save_tasks(tasks)
-            await dt_msg.reply("Готово. Пост запланирован")
-            dp.unregister_message_handler(get_datetime)
-        
-        dp.register_message_handler(get_datetime, state=None, chat_id=photo_msg.chat.id)
-        dp.unregister_message_handler(get_photo_inner)
+    dp.message.register(get_photo, lambda m: m.from_user.id == message.from_user.id)
 
-    dp.register_message_handler(get_photo_inner, state=None, chat_id=message.chat.id)
-    dp.unregister_message_handler(get_text)
+async def get_photo(message: types.Message):
+    photo_path = None
+    if message.content_type == ContentType.PHOTO:
+        file_info = await bot.get_file(message.photo[-1].file_id)
+        downloaded_file = await bot.download_file(file_info.file_path)
+        photo_path = os.path.join(photos_dir, f"{file_info.file_id}.jpg")
+        with open(photo_path, "wb") as f:
+            f.write(downloaded_file.read())
+    user_data[message.from_user.id]["photo"] = photo_path
+    await message.reply("Кинь дату и время в формате 2025-11-27 20:00")
+    dp.message.register(get_datetime, lambda m: m.from_user.id == message.from_user.id)
 
-# Фоновый автопостинг
+async def get_datetime(message: types.Message):
+    dt = datetime.strptime(message.text, "%Y-%m-%d %H:%M")
+    ts = int(dt.timestamp())
+    data = user_data.pop(message.from_user.id)
+    tasks = load_tasks()
+    tasks.append({
+        "chat_id": CHANNEL_ID,
+        "text": data["text"],
+        "photo": data["photo"],
+        "timestamp": ts
+    })
+    save_tasks(tasks)
+    await message.reply("Готово. Пост запланирован")
+
 async def scheduler():
     while True:
         tasks = load_tasks()
@@ -87,8 +86,9 @@ async def scheduler():
         save_tasks(new_tasks)
         await asyncio.sleep(2)
 
-# Запуск
+async def main():
+    asyncio.create_task(scheduler())
+    await dp.start_polling(bot)
+
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.create_task(scheduler())
-    executor.start_polling(dp)
+    asyncio.run(main())
